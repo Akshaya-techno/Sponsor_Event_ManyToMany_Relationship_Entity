78f91af11047494d1e4213365f900343f9f3f7b0
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-13 11:15:00 +0000

    termination symbol added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,14 +3,14 @@ VALUES
 ('TechCorp', 'Technology'),
 ('Glamour Inc.', 'Fashion'),
 ('SoundWave Productions', 'Music Production'),
-('EcoPlanet', 'Environmental Conservation')
+('EcoPlanet', 'Environmental Conservation');
 
 INSERT INTO event(name, date)
 VALUES
 ('TechCon', '2023-12-15'),
 ('Fashion Fest', '2023-11-05'),
 ('MusicFest', '2024-01-25'),
-('EcoAwareness Conclave', '2023-11-10')
+('EcoAwareness Conclave', '2023-11-10');
 
 INSERT INTO event_sponsor(sponsorId, eventId)
 VALUES
@@ -19,4 +19,4 @@ VALUES
 (2, 2),
 (3, 3),
 (3, 4),
-(4, 4)
+(4, 4);
