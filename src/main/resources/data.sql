9eb6f4c5ff9ecdd727b2c30a184a6c144b4e7c7b
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-13 11:08:07 +0000

    data populated

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO sponsor(name, industry)
+VALUES
+('TechCorp', 'Technology'),
+('Glamour Inc.', 'Fashion'),
+('SoundWave Productions', 'Music Production'),
+('EcoPlanet', 'Environmental Conservation')
+
+INSERT INTO event(name, date)
+VALUES
+('TechCon', '2023-12-15'),
+('Fashion Fest', '2023-11-05'),
+('MusicFest', '2024-01-25'),
+('EcoAwareness Conclave', '2023-11-10')
