fc0d78e39eaddfbceb7e55c76be7ae123a4f5989
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-14 05:27:09 +0000

    model sponsor.java commit

--- src/main/java/com/example/eventmanagementsystem/model/Sponsor.java
+++ src/main/java/com/example/eventmanagementsystem/model/Sponsor.java
@@ -9,3 +9,72 @@
  */
 
 // Write your code here
+package com.example.eventmanagementsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "sponsor")
+public class Sponsor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "industry")
+    private String industry;
+
+    @ManyToMany
+    @JoinTable(name = "event_sponsor", joinColumns = @JoinColumn(name = "sponsorId"), inverseJoinColumns = @JoinColumn(name = "eventId"))
+    @JsonIgnoreProperties("sponsors")
+    private List<Event> events = new ArrayList<>();
+
+    public Sponsor() {
+    }
+
+    public Sponsor(int id, String name, String industry) {
+        this.id = id;
+        this.name = name;
+        this.industry = industry;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setEvent(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Event> getEvent() {
+        return events;
+    }
+}
