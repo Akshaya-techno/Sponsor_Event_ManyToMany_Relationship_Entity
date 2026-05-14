046ef20b653a7d7331045c839063b68901b1b9c6
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-14 05:23:07 +0000

    model Event.java commit

--- src/main/java/com/example/eventmanagementsystem/model/Event.java
+++ src/main/java/com/example/eventmanagementsystem/model/Event.java
@@ -9,3 +9,70 @@
  */
 
 // Write your code here
+package com.example.eventmanagementsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date")
+    private String date;
+
+    @ManyToMany(mappedBy = "events")
+    @JsonIgnoreProperties("events")
+    private List<Sponsor> sponsors = new ArrayList<>();
+
+    public Event() {
+    }
+
+    public Event(int id, String name, String date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setSponsors(List<Sponsor> sponsors) {
+        this.sponsors = sponsors;
+    }
+
+    public List<Sponsor> getSponsors() {
+        return sponsors;
+    }
+}
